1179c6f985800064f2f3281aaa5d065105cd267c
Raymond Velasquez <dev@supermamon.com>
2023-03-01 17:44:04 +0800

    (new) DEBUG variable for troubleshooting

---
+++ app/crontest.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+echo "$(date) cron test" >> /data/logs/crontest.log
+
--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
-echo "* $0 starting dockcheck-web"
+
 
 export -p > /app/ENV
 
+[ "$DEBUG" = "true" ] && echo "*** DEBUGGING IS ACTIVATED ***" 
+
+echo "* $0 starting dockcheck-web"
+
 if [ "$NOTIFY" = "true" ]; then
     [ -n "$NOTIFY_URLS" ] && echo "* $0 notifications activated"
-    [ "$NOTIFY_DEBUG" = "true" ] && echo "$0 NOTIFY DEBUGMODE ACTIVATED"
 fi
 
 if [ "$CHECK_ON_LAUNCH" = "true" ]; then
@@ -23,12 +26,23 @@ chmod 777 /data
 [ -f "/data/containers" ] && chown www-data:www-data /data/*
 [ -f "/data/containers" ] && chmod 777 /data/*
 
+mkdir -p /data/logs || true
+
 echo "* $0 starting crontab"
-service cron start >/dev/null  2>&1 &
+if [ "$DEBUG" = "true" ]; then
+    echo "* DEBUG: $0 adding crontest to cron"
+    echo "  *  *  *  *  * root    /app/crontest.sh" >> /etc/crontab
+    service cron start
+else
+    service cron start >/dev/null  2>&1 &
+fi  
 
 echo "* $0 running watcher"
 /app/watcher.sh & #</dev/null >/dev/null 2>&1 &
 
 echo "* $0 starting web server"
+if [ "$SILENCE_APACHE_LOGS" = "true" ]; then
+  ln -fs /dev/null /var/log/apache2/access.log
+fi
 echo "ServerName localhost" >> /etc/apache2/apache2.conf
 exec apache2-foreground
