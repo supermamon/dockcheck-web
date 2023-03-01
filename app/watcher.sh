8b6ece0115fef754151091a836d90f3e4d22da1d
Raymond Velasquez <dev@supermamon.com>
2023-03-01 17:44:43 +0800

    (new) reduce watcher verbosity

--- app/watcher.sh
+++ app/watcher.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-while inotifywait -e modify /var/www/html/update.txt; do
+while inotifywait -q -e modify /var/www/html/update.txt; do
     echo "* $0 update trigerred."
     run-parts /etc/cron.daily/
     echo 0 > /var/www/html/update.txt
