cf2a1d455ae95ed5c8855993b35123f6c1b86cfb
Raymond Velasquez <dev@supermamon.com>
2023-03-06 22:57:31 +0800

    fix: dockcheck not running

--- app/watcher.sh
+++ app/watcher.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while inotifywait -q -e modify /var/www/html/update.txt; do
     echo "* $0 update trigerred."
-    run-parts /etc/cron.daily/
+    run-parts /etc/cron.custom/
     [ -f "/data/running" ] && rm /data/running
     echo 0 > /var/www/html/update.txt
 done
